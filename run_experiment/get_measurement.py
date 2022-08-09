64f3e6d98572db9c37010c2758d318bfe1fdb66b
Huanyu <huanyu.li@liu.se>
2022-08-09 16:26:42 +0200

    Update running experiments script and query names

--- run_experiment/get_measurement.py
+++ run_experiment/get_measurement.py
@@ -2,15 +2,17 @@ import sys
 import json
 import csv
 
-def get_measurement(number_of_runs, number_of_queries, log_path='/Users/huali50/Downloads/multiwayjoin_experiments/log/'):
+#group_names = {'Group1': '', 'Group2': '', 'Group3':'', 'Group4':'', 'Group5':''}
+
+def get_measurement(number_of_runs, number_of_queries, log_path):
   for query_id in range(number_of_queries):
     query_name = 'query' + str(query_id+1)
     measurements = []
-    with open('./query{id}.csv'.format(id=query_id+1), 'w', encoding='UTF8') as csv_file:
+    with open('{log_path}query{id}.csv'.format(log_path=log_path, id=query_id+1), 'w', encoding='UTF8') as csv_file:
       writer = csv.writer(csv_file)
       writer.writerow(['query', 'num_of_run', 'overallQueryProcessingTime(ms)'])
       for run in range(number_of_runs):
-        log_file = '{log_path}{query_name}-{run}.txt'.format(log_path=log_path, query_name=query_name, run=run+1)
+        log_file = '{log_path}{query_name}-{run}.log'.format(log_path=log_path, query_name=query_name, run=run+1)
         content = []
         with open(log_file) as f:
           content = f.readlines()
@@ -23,4 +25,5 @@ def get_measurement(number_of_runs, number_of_queries, log_path='/Users/huali50/
 if __name__ == "__main__":
   number_of_runs = int(sys.argv[1])
   number_of_queries = int(sys.argv[2])
-  get_measurement(number_of_runs, number_of_queries)
+  log_path = str(sys.argv[3])
+  get_measurement(number_of_runs, number_of_queries, log_path)
--- run_experiment/run_experiment.sh
+++ run_experiment/run_experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-log_path="./logs/"
+
 
 declare endpoints
 export query_num=$1
@@ -10,12 +10,13 @@ echo $number_of_runs
 export federation_num=$3
 export engine=$4
 export queries_path=$5
-#export logs_path=$6
-export endpoint=$6
+export log_path=$5
+export approach_option=$6
+export endpoint=$7
 
 for fed in $(seq 1 $federation_num)
 do
-  let ep=$fed+5
+  let ep=$fed+6
   #echo ${!ep}
   endpoints+="--considerSPARQLEndpoint "
   endpoints+=${!ep}
@@ -27,21 +28,21 @@ done
 
 echo $endpoints
 #echo $query_num
-mkdir logs
+#mkdir logs
 for run in $(seq 1 $number_of_runs)
 do
   for query_id in $(seq 1 $query_num)
   do
     #echo ${query_path}query${query_id}.rq
-    export log_file_path=${log_path}query${query_id}-${run}.txt
+    export log_file_path=${log_path}query${query_id}-${run}.log
     #java -cp $HeFQUIN se.liu.ida.hefquin.cli.RunQueryWithoutSrcSel --query ${query_path}query${query_id}.rq  --considerSPARQLEndpoint http://10.253.227.81:9999/bigdata/sparql > $log_file_path
     #java -cp $HeFQUIN se.liu.ida.hefquin.cli.RunQueryWithoutSrcSel --query ${query_path}query${query_id}.rq  --considerSPARQLEndpoint $endpoint > $log_file_path
     #java -cp $HeFQUIN se.liu.ida.hefquin.cli.RunQueryWithoutSrcSel --query ${query_path}query${query_id}.rq  --considerSPARQLEndpoint $endpoint > $log_file_path
     #java -cp $engine se.liu.ida.hefquin.cli.RunQueryWithoutSrcSel --query ${queries_path}query${query_id}.rq  --considerSPARQLEndpoint $endpoints > $log_file_path
-    java -cp $engine se.liu.ida.hefquin.cli.RunQueryWithoutSrcSel --query ${queries_path}query${query_id}.rq  $endpoints > $log_file_path
+    java -cp $engine se.liu.ida.hefquin.cli.RunQueryWithoutSrcSel --query ${queries_path}query${query_id}.sparql  $endpoints > $log_file_path
   done
 done
-python get_measurement.py $number_of_runs $query_num
+python get_measurement.py $number_of_runs $query_num $log_path
 
 
 
