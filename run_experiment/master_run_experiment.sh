f572ea40f195b3f2c13ecd193881e2e0c2ae9b34
Julián Arenas Guerrero <18464038+ArenasGuerreroJulian@users.noreply.github.com>
2022-08-10 11:05:11 +0200

    Create master_run_experiment.sh

---
+++ run_experiment/master_run_experiment.sh
@@ -0,0 +1,19 @@
+./run_experiment.sh ../queries/group1/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 1
+./run_experiment.sh ../queries/group1/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 2
+./run_experiment.sh ../queries/group1/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 3
+
+./run_experiment.sh ../queries/group2/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 1
+./run_experiment.sh ../queries/group2/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 2
+./run_experiment.sh ../queries/group2/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 3
+
+./run_experiment.sh ../queries/group3/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 1
+./run_experiment.sh ../queries/group3/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 2
+./run_experiment.sh ../queries/group3/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 3
+
+./run_experiment.sh ../queries/group4/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 1
+./run_experiment.sh ../queries/group4/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 2
+./run_experiment.sh ../queries/group4/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 3
+
+./run_experiment.sh ../queries/group5/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 1
+./run_experiment.sh ../queries/group5/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 2
+./run_experiment.sh ../queries/group6/ 10 ../HeFQUIN-0.0.1-SNAPSHOT.jar 3
